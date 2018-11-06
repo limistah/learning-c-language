e2915657e740e6c3a5283c39ac24f41a930fce1f
Aleem Isiaka <aleemisiaka@gmail.com>
2018-11-06 19:34:40 +0100

    Uses define for program constants

--- K And R/chapter-one/fahr-cel-rev.c
+++ K And R/chapter-one/fahr-cel-rev.c
@@ -4,20 +4,19 @@
 Print Fahrenheit to celsius table for fahr = 20, 40, 60 , 80 ... 300
 */
 
+#define LOWER 0   /* lower limit of temperature table */
+#define UPPER 300 /* upper limit */
+#define STEP 20   /* Step size */
+
 main()
 {
   float fahr, celsius;
-  int lower, upper, step;
 
-  lower = 0;   /* lower limit of temperature table */
-  upper = 300; /* upper limit */
-  step = 20;   /* Step size */
-
-  fahr = lower;
+  fahr = LOWER;
   printf("F\t   Cel\n");
   printf("==============\n");
   // Exercise 1.5 : Reversed temperature range
-  for (fahr = upper; fahr >= lower; fahr = fahr - step)
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
   {
     celsius = (5.0 / 9.0) * (fahr - 32);
     printf("%3.0f\t%6.1f\n", fahr, celsius);
